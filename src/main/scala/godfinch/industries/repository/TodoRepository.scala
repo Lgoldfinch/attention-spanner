5dceedf1bf65ddc57f0c72ae5838fb51abdded94
Louis Goldfinch <louis.goldfinch@qomplx.com>
2023-06-26 16:58:47 +0100

    Created db query for insertTodoList

--- src/main/scala/godfinch/industries/repository/TodoRepository.scala
+++ src/main/scala/godfinch/industries/repository/TodoRepository.scala
@@ -1,8 +1,8 @@
 package godfinch.industries.repository
 
-import cats.Applicative
-import cats.effect.Resource
+import cats.effect.{MonadCancelThrow, Resource}
 import godfinch.industries.hello._
+import cats.implicits._
 import java.util.UUID
 import skunk._
 import skunk.implicits._
@@ -19,37 +19,29 @@ trait TodoRepository[F[_]] {
   def updateTodoList(todoList: TodoList): F[Unit]
 }
 
-final class TodoRepositoryImpl[F[_]](postgres: Resource[F, Session[F]])(implicit A: Applicative[F]) extends TodoRepository[F] {
+final class TodoRepositoryImpl[F[_]: MonadCancelThrow](postgres: Resource[F, Session[F]]) extends TodoRepository[F] {
   val todoListDecoder: Decoder[TodoList] =  (todoListId *: todoListName *: timeCreated *: todoNames).to[TodoList]
 
-  val getTodoListById =
-   sql"""
-      select id, name, created_timestamp, tasks from todos
-      where id = $todoListId
-      """.query(todoListDecoder)
+
 
   val todoId = TodoListId(UUID.randomUUID())
 
-  val todoName = TodoListName("Leaving for work")
+import TodoRepositoryImpl._
 
-  val todos = List(TodoName("Bring wallet"), TodoName("Bring laptop"), TodoName("Bring Keys"))
-
-  override def insertTodoList(todoList: TodoList): F[Unit] = ???
+  override def insertTodoList(todoList: TodoList): F[Unit] = {
+    todoList match {
+      case TodoList(id, todoListName, created, todos) =>
+        postgres.use { session =>
+          session.prepare(insertTodoListCommand).flatMap { cmd =>
+            cmd.execute(id *: todoListName *: created *: todos *: EmptyTuple).void
+          }
+        }
+    }
+  }
 
   override def getAllTodoLists: F[AllTodoListsB] = {
     val todos = List(TodoName("Bring wallet"), TodoName("Bring laptop"), TodoName("Bring Keys"))
-
-
 ???
-//    AllTodoListsB(
-//    List(
-//      TodoList(
-//        todoName,
-//        todos,
-//        None,
-//        )
-//    )
-//    ).pure[F]
   }
 
   override def getTodoList(todoListId: TodoListId): F[TodoList] = {
@@ -69,4 +61,22 @@ final class TodoRepositoryImpl[F[_]](postgres: Resource[F, Session[F]])(implicit
 //  ).pure[F]
 
   override def updateTodoList(todoList: TodoList): F[Unit] = A.unit
+}
+
+object TodoRepositoryImpl {
+  val insertTodoListCommand: Command[TodoListId *: TodoListName *: TimeCreated *: List[TodoName] *: EmptyTuple] = {
+    sql"""
+        insert into todos (id, name, created_timestamp, tasks)
+        values ($todoListId, $todoListName, $timeCreated, $todoNames)
+       """.command.contramap {
+      case id *: todoName *: timeCreated *: todos *: EmptyTuple =>
+        id *: todoName *: timeCreated *: todos *: EmptyTuple
+    }
+  }
+
+  val getTodoListById =
+    sql"""
+      select id, name, created_timestamp, tasks from todos
+      where id = $todoListId
+      """.query(todoListDecoder)
 }

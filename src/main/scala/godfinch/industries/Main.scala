119a1c77ac7cd0f4c5c807e091fba8af9b20052a
legoldfinch <louisgoldfinch@gmail.com>
2023-09-20 15:30:32 +0100

    extracted Postgres setup into a separate file.

--- src/main/scala/godfinch/industries/Main.scala
+++ src/main/scala/godfinch/industries/Main.scala
@@ -13,36 +13,26 @@ import skunk.implicits._
 import skunk.codec.all._
 import cats.effect._
 import cats.effect.unsafe.implicits.global
+import godfinch.industries.resources.AppResources
 import skunk._
 import skunk.implicits._
 import skunk.codec.all._
 import natchez.Trace.Implicits.noop
-import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.{Logger, SelfAwareStructuredLogger}
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 object Main extends IOApp {
-
-  val session: Resource[IO, Resource[IO, Session[IO]]] =
-    Session.pooled[IO]( // (2)
-      host = "database",
-      port = 5432,
-      user = "postgres",
-      database = "postgres",
-      password = Some("example"),
-      max = 16
-    )
-
   override def run(args: List[String]): IO[ExitCode] = {
 
     val thePort = port"8080"
     val theHost = host"0.0.0.0"
+    implicit val logger: SelfAwareStructuredLogger[IO] = Slf4jLogger.getLogger[IO]
 
     for {
-      postgres <- session.evalTap(checkPostgresConnection)
-      implicit0(logger: Logger[IO]) <- Resource.eval(Slf4jLogger.create[IO])
+      resources <- AppResources.make[IO]
       sqlMigrator = new SqlMigrator[IO]("jdbc:postgresql://database:5432/postgres")
       _ <- Resource.eval(sqlMigrator.run)
-      routes <- Routes.all(postgres)
+      routes <- Routes.all(resources.postgres)
 
       _      <- EmberServerBuilder
       .default[IO]
@@ -53,12 +43,5 @@ object Main extends IOApp {
     } yield ()
   }.useForever
 
-  def checkPostgresConnection(
-                               postgres: Resource[IO, Session[IO]]
-                             ): IO[Unit] =
-    postgres.use { session =>
-      session.unique(sql"select version();".query(text)).flatMap { v =>
-        IO.println(s"Connected to Postgres $v. *******")
-      }
-    }
+
 }
---
+++ src/main/scala/godfinch/industries/resources/AppResources.scala
@@ -0,0 +1,41 @@
+package godfinch.industries.resources
+
+import cats.effect.kernel.Temporal
+import cats.effect.std.Console
+import cats.effect.{MonadCancelThrow, Resource}
+import fs2.io.net.Network
+import natchez.Trace.Implicits.noop
+import org.typelevel.log4cats.Logger
+import skunk.Session
+import skunk.codec.all.text
+import skunk.implicits._
+
+sealed abstract class AppResources[F[_]](
+                                          val postgres: Resource[F, Session[F]]
+                                        )
+
+object AppResources {
+  def make[F[_]: Console: Logger: MonadCancelThrow: Network: Temporal]: Resource[F, AppResources[F]] = {
+      def checkPostgresConnection(
+                                         postgres: Resource[F, Session[F]]
+                                       ): F[Unit] =
+      postgres.use { session =>
+        session.unique(sql"select version();".query(text)).flatMap { v =>
+          Logger[F].info(s"Connected to Postgres $v.")
+        }
+      }
+
+    def mkPostgresResource: Resource[F, Resource[F, Session[F]]] =
+      Session.pooled[F](
+        host = "database",
+        port = 5432,
+        user = "postgres",
+        database = "postgres",
+        password = Some("example"),
+        max = 16
+      ).evalTap(checkPostgresConnection)
+
+
+    mkPostgresResource.map(new AppResources[F](_) {})
+  }
+}

3b082e5df8a661c32068ff93f85984e3a6550f72
lgoldfinch <louis.goldfinch@3ds.com>
2024-02-03 12:10:42 +0000

    Have our first line

--- ignore_build_if.sh
+++ ignore_build_if.sh
@@ -7,27 +7,51 @@ file_path=".scalafmt.conf"
 commit_sha="$1"
 github_token="$2"
 
-echo "hello"
-echo "$1"
-
-
 # GitHub API URL for comparing commits
-api_url="https://api.github.com/repos/$repo_owner/$repo_name/compare/$commit_sha...HEAD"
+#api_url="https://api.github.com/repos/$repo_owner/$repo_name/compare/$commit_sha...HEAD"
+api_url="https://api.github.com/repos/Lgoldfinch/attention-spanner/commits/$commit_sha"
+
+#echo "$response"
 
 # Get the file details from the GitHub API
-file_details=$(curl -s -H "Authorization: token $github_token" $api_url | grep -o "\"filename\":\"$file_path\",\".*\"status\":\"[a-z]*\"" | head -1)
 
-echo
-# Check if the file exists in the comparison result
-if [ ! -z "$file_details" ]; then
-    # Extract the file status using string manipulation
-    file_status=$(echo "$file_details" | grep -o "\"status\":\"[a-z]*\"" | cut -d':' -f2 | tr -d '"')
+#echo "$commit_compare_result"
 
-    if [ "$file_status" == "modified" ]; then
-        echo "true"  # File has been modified
-    else
-        echo "false"  # File has not been modified
-    fi
-else
-    echo "File not found in the comparison result."
+fileName=$(curl -s -H "Authorization: token $github_token" $api_url | jq -r '.files.[].filename')
+fileLength=$(curl -s -H "Authorization: token $github_token" $api_url | jq -r '.files' | jq 'length')
+fileStatus=$(curl -s -H "Authorization: token $github_token" $api_url | jq -r '.files.[].status')
+
+echo "$fileName"
+echo "$file_path"
+echo "$fileLength"
+echo "$fileStatus"
+#res=$(curl -s -H "Authorization: token $github_token" $api_url)
+
+if [ "$fileName" == "$file_path" ]
+then
+  echo hello
 fi
+
+#echo "$res"
+
+#getCommitUrl=$(commit_compare_result | grep -o )
+
+
+
+#| grep -o "\"filename\":\"$file_path\",\".*\"status\":\"[a-z]*\"" | head -1)
+
+#echo "$commit_compare_result" | grep -o
+#echo
+# Check if the file exists in the comparison result
+#if [ ! -z "$file_details" ]; then
+#     Extract the file status using string manipulation
+#    file_status=$(echo "$file_details" | grep -o "\"status\":\"[a-z]*\"" | cut -d':' -f2 | tr -d '"')
+#
+#    if [ "$file_status" == "modified" ]; then
+#        echo "true"  # File has been modified
+#    else
+#        echo "false"  # File has not been modified
+#    fi
+#else
+#    echo "File not found in the comparison result."
+#fi
